a843536dd4544a0de27b0c18606fc415e375e186
Alexey Lopukh <lopukh911@gmail.com>
2020-01-28 21:46:46 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/lopukh/mitso_messanger/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lopukh.mitso_messanger
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
